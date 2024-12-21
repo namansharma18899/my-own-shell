89c612e74945d8ef66aa2f94a1c8fe2cff41e792
Naman Sharma <namansharma18899@gmail.com>
2024-12-21 17:21:28 +0530

    [any message]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,22 +4,23 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
 func main() {
-	force_close_prg_flag := false
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		if force_close_prg_flag {
-			os.Exit(1)
-		}
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "error reading input:", err)
 			// os.Exit(1)
+		}
+		input := strings.TrimSpace(command)
+		if input == "exit 0" {
+			os.Exit(1)
 		}
 		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
