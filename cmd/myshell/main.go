7696809a37a168772a22fac994ca8b0462a59592
Naman Sharma <namansharma18899@gmail.com>
2024-12-21 17:06:07 +0530

    [any message]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,14 +10,17 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
-
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "error reading input:", err)
-		os.Exit(1)
+	force_close_prg_flag := false
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		if force_close_prg_flag {
+			os.Exit(1)
+		}
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "error reading input:", err)
+			// os.Exit(1)
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
